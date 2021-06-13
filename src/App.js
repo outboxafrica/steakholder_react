fa198dc5e9f55bfa0db15a9476ac99593fc45ed0
Murungi Matthew <murungimatthew@Murungi.local>
2021-06-13 23:13:53 +0300

    create home-component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import React, { Component } from 'react'
 import Navigation from './Navigation/Navigation';
+import Home from './Home/Home';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navigation />
+        <Home />
       </div>
     )
   }
