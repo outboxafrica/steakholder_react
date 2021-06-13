322eaafb04da3760f29591b2de4f2d3649f4bc90
Murungi Matthew <murungimatthew@Murungi.local>
2021-06-14 00:42:06 +0300

    add header-component

---
+++ src/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+import './Header.css'
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+            <div className="header__detail">
+              <p className="header__detailTitle">
+                Eating Vegetables is a Mis-steak, Let’s save the planet
+              </p>
+              <p className="header__detailSubtitle">
+                We consume less vegetables and eat the very things that cause
+                global warming
+              </p>
+              <button className="actionButton">Explore More</button>
+            </div>
+            <img
+              className="header__Image"
+              src="https://image.freepik.com/free-vector/female-friends-hanging-out-cafe_74855-6459.jpg"
+              alt=""
+              srcset=""
+            />
+            </div>
+        )
+    }
+}
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Header from "../Header/Header";
 import MeatCategory from "../MeatCategory/MeatCategory";
 
 import "./Home.css";
@@ -7,26 +8,9 @@ export default class Home extends Component {
   render() {
     return (
       <div className="home">
-        <div className="home__header">
-          <div className="home__headerDetail">
-            <p className="home__headerDetailTitle">
-              Eating Vegetables is a Mis-steak, Let’s save the planet
-            </p>
-            <p className="home__headerDetailSubtitle">
-              We consume less vegetables and eat the very things that cause
-              global warming
-            </p>
-            <button className="actionButton">Explore More</button>
-          </div>
-          <img
-            className="home__headerImage"
-            src="https://image.freepik.com/free-vector/female-friends-hanging-out-cafe_74855-6459.jpg"
-            alt=""
-            srcset=""
-          />
-        </div>
+    
 
-     
+     <Header />
 <MeatCategory />
       </div>
 
